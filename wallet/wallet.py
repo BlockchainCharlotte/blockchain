3236a98ca71b1911d1286a9b029dd67a7d254f67
Lance Rogers <lancekincaid1994@gmail.com>
2018-01-08 18:42:25 -0500

    updated wallet.py

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -15,13 +15,23 @@ class Wallet(object):
                                         #                transaction_id)
         self.balance = 0
 
-    def load_wallet_file(self):
-        """Loads key pairs and addresses from
-        wallet file"""
-        pass
+    def load_wallet_file(self, wallet, path):
+        """
+        Loads key pairs and addresses from
+        wallet file
+        :param wallet: <object> Wallet Object
+        :param path: <str> Path to wallet file
+        """
+        wallet_f = open(path, 'r')
+        pickle.load(wallet_f)
+        wallet_f.close()
 
     def write_wallet_file(self, wallet, file_name="wallet.dat"):
-        """Pickles Wallet"""
+        """
+        Pickles Wallet
+        :param wallet: <object> Wallet Object
+        :file_name: <str>  Optional wallet name
+        """
         wallet_obj = open(file_name, 'wb')
         pickle.dump(wallet, wallet_obj)
         wallet_obj.close()
@@ -30,6 +40,7 @@ class Wallet(object):
         """
         Search blockchain for transactions signed with
         public key in self.public_keys
+        :param blockchain: <list> A blockchain
         """
         pass
 
@@ -37,7 +48,8 @@ class Wallet(object):
         key_pair_address = addrgen()
         self.key_pair_address.append(key_pair_address)
         self.active_kpa = key_pair_address
-        return 
+        print(key_pair_address)
+        return key_pair_address
 
     def sign(private_key, msg):
         sk = ecdsa.SigningKey.from_string(codecs.decode(private_key, "hex"),
